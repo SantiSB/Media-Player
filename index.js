a84fcfee51a049d4e81b243d59085b4bf628ce7a
santiagoprueba <santiago@gmail.com>
2022-11-29 22:57:36 -0500

    route index config

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay.js';
+import MediaPlayer from './assets/MediaPlayer.js';
+import AutoPlay from './assets/plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
